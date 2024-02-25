26b7c6348c2c00779e9f32855d454c48d900260a
Coocit <398727436@qq.com>
2024-02-26 00:23:26 +0800

    谷歌图形验证码引入并开发验证码接口

--- cloud-account/src/main/java/com/coocit/controller/NotifyController.java
+++ cloud-account/src/main/java/com/coocit/controller/NotifyController.java
@@ -1,10 +1,22 @@
 package com.coocit.controller;
 
 import com.coocit.service.NotifyService;
+import com.coocit.utils.CommonUtil;
+import com.google.code.kaptcha.Producer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author: Coocit
@@ -17,6 +29,57 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotifyController {
 
     @Autowired
+    private Producer captchaProducer;
+
+    @Autowired
     private NotifyService notifyService;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    /**
+     * 验证码过期时间
+     */
+    private static final long CAPTCHA_CODE_EXPIRED = 1000 * 10 * 60;
+
+
+    /**
+     * 生成验证码
+     *
+     * @param request
+     * @param response
+     */
+    @GetMapping("captcha")
+    public void getCaptcha(HttpServletRequest request, HttpServletResponse response) {
+
+
+        String captchaText = captchaProducer.createText();
+        log.info("验证码内容:{}", captchaText);
+
+        //存储redis,配置过期时间 ， jedis/lettuce
+        redisTemplate.opsForValue().set(getCaptchaKey(request), captchaText, CAPTCHA_CODE_EXPIRED, TimeUnit.MILLISECONDS);
+
+
+        BufferedImage bufferedImage = captchaProducer.createImage(captchaText);
+
+        try (ServletOutputStream outputStream = response.getOutputStream()) {
+
+            ImageIO.write(bufferedImage, "jpg", outputStream);
+            outputStream.flush();
+        } catch (IOException e) {
+            log.error("获取流出错:{}", e.getMessage());
+        }
+
+    }
+
+    private String getCaptchaKey(HttpServletRequest request) {
+
+        String ip = CommonUtil.getIpAddr(request);
+        String userAgent = request.getHeader("User-Agent");
+        String key = "account-service:captcha:" + CommonUtil.MD5(ip + userAgent);
+        log.info("验证码key:{}", key);
+        return key;
+    }
+
 
 }
