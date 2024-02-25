fe9b55203bd55220dba116b46acc9468f46fea00
Coocit <398727436@qq.com>
2024-02-26 00:04:20 +0800

    sms组件异步化

--- cloud-account/src/main/java/com/coocit/controller/NotifyController.java
+++ cloud-account/src/main/java/com/coocit/controller/NotifyController.java
@@ -1,6 +1,8 @@
 package com.coocit.controller;
 
+import com.coocit.service.NotifyService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/notify/v1")
 @Slf4j
 public class NotifyController {
+
+    @Autowired
+    private NotifyService notifyService;
 
 }
